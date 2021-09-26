88959681072fe84ea39b8fda1443be952cf765e5
ZaidebinUsama <77499086+usama-khalied@users.noreply.github.com>
2021-09-27 03:40:08 +0500

    abhi hai

---
+++ script.js
@@ -0,0 +1,41 @@
+const text = document.querySelector(".fancy");
+
+const strText = text.textContent;
+
+const splitText = strText.split("");
+
+text.textContent="";
+for(let i=0; i < splitText.length; i++){
+
+text.innerHTML += "<span>" + splitText[i] +  "</span>";
+
+}
+let char = 0;
+
+let timer = setInterval(myfun, 30)
+
+function myfun() {
+
+const span = document.querySelectorAll('span')[char];
+
+span.classList.add("fade");
+
+char++;
+
+if(char === splitText.length){
+
+    complete();
+
+    return;
+
+}
+
+}
+
+function complete() {
+
+clearInterval(timer);
+
+timer = null;
+
+}
